1cb661e3ce6fe548d7b7b29e67d176266c10a8d3
moraxborax <humphrey57shen@outlook.com>
2025-11-28 10:41:57 +0800

    error handle

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
-
+	"log"
 	"moraxborax/greetings"
 )
 
-func main(){
-	message := greetings.Hello("Gladys")
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//message := greetings.Hello("Gladys")
 	fmt.Println(message)
 }
